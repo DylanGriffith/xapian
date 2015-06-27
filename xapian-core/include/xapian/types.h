9d66193127a8de1047debd8b8b59990798b69147
Dylan Griffith <dyl.griffith@gmail.com>
2015-06-27 16:56:01 +1000

    use unsigned long long in types.h when configured

--- xapian-core/include/xapian/types.h
+++ xapian-core/include/xapian/types.h
@@ -25,6 +25,7 @@
 # error "Never use <xapian/types.h> directly; include <xapian.h> instead."
 #endif
 
+#include <xapian/version.h>
 #include <xapian/deprecated.h>
 
 namespace Xapian {
@@ -34,21 +35,33 @@ namespace Xapian {
  *  This is used to hold values such as the number of documents in a database
  *  and the frequency of a term in the database.
  */
+#ifdef XAPIAN_HAS_64BIT_DOCID
+typedef unsigned long long doccount;
+#else
 typedef unsigned doccount;
+#endif
 
 /** A signed difference between two counts of documents.
  *
  *  This is used by the Xapian classes which are STL containers of documents
  *  for "difference_type".
  */
-typedef int doccount_diff; /* FIXME: can overflow with more than 2^31 docs. */
+#ifdef XAPIAN_HAS_64BIT_DOCID
+typedef long long doccount_diff;
+#else
+typedef int doccount_diff;
+#endif
 
 /** A unique identifier for a document.
  *
  *  Docid 0 is invalid, providing an "out of range" value which can be
  *  used to mean "not a valid document".
  */
+#ifdef XAPIAN_HAS_64BIT_DOCID
+typedef unsigned long long docid;
+#else
 typedef unsigned docid;
+#endif
 
 /** A normalised document length.
  *
@@ -68,14 +81,22 @@ XAPIAN_DEPRECATED(typedef int percent);
  *
  *  This is used to hold values such as the Within Document Frequency (wdf).
  */
+#ifdef XAPIAN_HAS_64BIT_TERMCOUNT
+typedef unsigned long long termcount;
+#else
 typedef unsigned termcount;
+#endif
 
 /** A signed difference between two counts of terms.
  *
  *  This is used by the Xapian classes which are STL containers of terms
  *  for "difference_type".
  */
-typedef int termcount_diff; /* FIXME: can overflow with more than 2^31 terms. */
+#ifdef XAPIAN_HAS_64BIT_TERMCOUNT
+typedef long long termcount_diff;
+#else
+typedef int termcount_diff;
+#endif
 
 /** A term position within a document or query.
  */
